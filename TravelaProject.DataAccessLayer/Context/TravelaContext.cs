7001df4f303d099a3a91fd34a6e7df38e14d65e0
Türkay Sağır <ates_turkay@hotmail.com>
2024-10-13 17:53:08 +0300

    Context dosyası oluşturuldu ve N-Katmanlı Mimari için klasörleme yapıldı.

---
+++ TravelaProject.DataAccessLayer/Context/TravelaContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelaProject.EntityLayer.Concrete;
+
+namespace TravelaProject.DataAccessLayer.Context
+{
+    public class TravelaContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("SERVER=DESKTOP-ER46C3V;initial catalog=DbTravelaProject;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+
+
+    }
+}
